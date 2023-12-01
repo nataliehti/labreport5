2ba25fedee894b4f221fc1db37ef4d9c33c2348b
nataliehti <146873462+nataliehti@users.noreply.github.com>
2023-12-01 15:01:55 -0800

    Update ListExamplesTests.java

--- ListExamplesTests.java
+++ ListExamplesTests.java
@@ -1,22 +1,45 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-import java.util.*;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+class CheckApple implements StringChecker {
+  public boolean checkString(String s) {
+    return s.equalsIgnoreCase("apple");
+  }
+}
 
 public class ListExamplesTests {
-	@Test(timeout = 500)
-	public void testMerge1() {
-    		List<String> l1 = new ArrayList<String>(Arrays.asList("x", "y"));
-		List<String> l2 = new ArrayList<String>(Arrays.asList("a", "b"));
-		assertArrayEquals(new String[]{ "a", "b", "x", "y"}, ListExamples.merge(l1, l2).toArray());
-	}
-	
-	@Test(timeout = 500)
-        public void testMerge2() {
-		List<String> l1 = new ArrayList<String>(Arrays.asList("a", "b", "c"));
-		List<String> l2 = new ArrayList<String>(Arrays.asList("c", "d", "e"));
-		assertArrayEquals(new String[]{ "a", "b", "c", "c", "d", "e" }, ListExamples.merge(l1, l2).toArray());
-        }
+  @Test(timeout = 500)
+  public void testMergeRightEnd() {
+    List<String> left = Arrays.asList("a", "b", "c");
+    List<String> right = Arrays.asList("a", "d");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+    assertEquals(expected, merged);
+  }
 
+  @Test(timeout = 500)
+  public void testFilter() {
+    List<String> input1 = Arrays.asList("apple", "banana", "Apple");
+    CheckApple sc = new CheckApple();
+    List<String> output1 = ListExamples.filter(input1, sc);
+    assertEquals(
+      Arrays.asList(new String[] { "apple", "Apple"}),
+      output1
+    );
+  }
+
+  @Test(timeout = 500)
+  public void testMerge() {
+    List<String> input1 = Arrays.asList("c", "e", "f", "g");
+    List<String> input2 = Arrays.asList("a", "b", "e", "j");
+    List<String> output1 = ListExamples.merge(input1, input2);
+    assertEquals(
+      Arrays.asList(new String[] { "a", "b", "c", "e", "e", "f", "g", "j" }),
+      output1
+    );
+  }
+
+  // todo add more tests
 }
